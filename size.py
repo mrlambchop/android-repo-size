b9669a0ae4973db063f7aca5c8b8e6893108c19b
Nick Lambourne <npl@broadcom.com>
2013-03-24 22:06:36 -0700

    Working with first stats output
    Signed-off-by: Nick Lambourne <npl@broadcom.com>

--- size.py
+++ size.py
@@ -2,19 +2,27 @@ import os, string, sys, shutil
 from subprocess import call
 import csv
 
+#Function to check file counts per directory
+#
+#find ./ -type f | grep -E ".*\.[a-zA-Z0-9]*$" | sed -e 's/.*\(\.[a-zA-Z0-9]*\)$/\1/' | sort | uniq -c | sort -n
+
 #android_branches = [ 'android-1.5r4', 'android-1.6_r2', 'android-2.1_r2.1s', 'android-2.2.3_r2.1', 'android-2.3.7_r1', 'android-4.0.4_r2.1', 'android-4.1.2_r1', 'android-4.2.2_r1' ]
 #android_name = [ 'cupcake', 'donut', 'eclair', 'froyo', 'gingerbread', 'icecream-sandwidth', 'jellybean 4.1', 'jellybean 4.2' ]
 
 android_branches = [ 'android-1.6_r2', 'android-2.1_r2.1s', 'android-2.2.3_r2.1', 'android-2.3.7_r1', 'android-4.0.4_r2.1', 'android-4.1.2_r1', 'android-4.2.2_r1' ]
 android_name = [ 'donut', 'eclair', 'froyo', 'gingerbread', 'icecream-sandwidth', 'jellybean 4.1', 'jellybean 4.2' ]
 
-def PrintBranchNameInfo():
-   for count,branch in enumerate( android_branches ):
-      print branch, android_name[count]
+cloc_columns = [ 'files', 'language', 'blank', 'comment', 'code' ]
+
+classification = [ 'native', 'build_and_tools', 'framework', 'apps', 'dev' ]
+
+file_types = [ 'java', 'native', 'build_scripts', 'xml' ]
+
 
 def CreateDirectory( path ):
    shutil.rmtree(path, True )
    os.makedirs(path)
+
 
 def CheckoutRepo( path, branch ):
    CreateDirectory( path )
@@ -27,7 +35,9 @@ def CheckoutRepo( path, branch ):
    cmd = "repo sync"
    os.system( cmd )
 
+
 def StripGitRepos( path ):
+
    os.chdir(path)
 
    #get the big guy first...
@@ -41,6 +51,7 @@ def StripGitRepos( path ):
             print "Deleteing: ", p
             shutil.rmtree( p )
 
+
 def HighLevelDirs( path ):
    out = []
    for item in os.listdir(path):
@@ -48,17 +59,19 @@ def HighLevelDirs( path ):
          out.append( item )
    return out
 
+
 def ParseClocResults( filename ):   
-    fields = [ 'files', 'language', 'blank', 'comment', 'code' ]  
-    #fields = [ "XML", "Java", "C++", "HTML", "C/C++ Header", "C", "Python", "Javascript", "Assembly", "make", "Bourne Shell", "Bourne Again Shell" ]
+    #create a dict for the 4 columns
 
     d = {}
-    for f in fields:
+    for f in cloc_columns:
        d[f] = []
 
-    dictReader = csv.DictReader(open(filename, 'rb'), fieldnames = fields, delimiter = ',', quotechar = '"')
+    #read the data from the CSV file
+    dictReader = csv.DictReader(open(filename, 'rb'), fieldnames = cloc_columns, delimiter = ',', quotechar = '"')
     dictReader.next()
 
+    #for each row of the CSV data returned, add to the column dict
     for row in dictReader:
        for key in row:
           try:
@@ -69,12 +82,14 @@ def ParseClocResults( filename ):
     return d
 
 
+#returns a Dict with key for the high level directory, the value being a dict of the file types and breakdown (lines, comments etc...)
 def CountLinesOfCode( path, branch, high_level_dirs ):
    out = {}
+
    for d in high_level_dirs:
       tmpfile = path + "/" + d + ".txt"
-      cmd = "cloc " + branch + "/" + d + " --quiet --csv --progress-rate=0 --report-file=" + tmpfile
-      print cmd
+      cmd = "cloc " + branch + "/" + d + " --quiet --csv --progress-rate=0 --force-lang=\"make\",mk --report-file=" + tmpfile
+      #print cmd
       
       if not os.path.exists( tmpfile ):
          os.system( cmd )
@@ -87,29 +102,139 @@ def CountLinesOfCode( path, branch, high_level_dirs ):
    return out
 
 
-#####
+#output dict is a list of src files
+def ParseStats( dir_dict, output_dict ):
 
-PrintBranchNameInfo()
+   if len(dir_dict) == 0:
+      return
 
-initial_path = os.getcwd()
+   files = dir_dict['files']
+   language = dir_dict['language']
+   blank = dir_dict['blank']
+   comment = dir_dict['comment']
+   code = dir_dict['code']
 
-#checkout the branches
-for branch in android_branches:
-   branch_path = initial_path + "/" + branch
-   print branch_path
+   java_l = [ 'Java' ] #'Javascript'
+   native_l = [ 'C++', 'C/C++ Header', 'C', 'Assembly' ]
+   build_scripts_l = [ 'Python', 'make', 'Bourne Shell', 'Bourne Again Shell' ]
+   xml_l = [ 'XML' ]
 
-   if not os.path.exists( branch_path ):
-      print "Branch does not exist - creating"
-      CheckoutRepo( branch_path, branch )
-      StripGitRepos( branch_path ) #this is just to save space
+   d = files
 
-#get all the top level dirs from the last branch
-last_branch_path = initial_path + "/" + android_branches[-1]
-high_level_dirs = HighLevelDirs( last_branch_path )
+   for c, l in enumerate(language):
+      if l in java_l:
+         output_dict['java'] = output_dict['java'] + int(d[c])
+      elif l in native_l:
+         output_dict['native'] = output_dict['native'] + int(d[c])
+      elif l in build_scripts_l:
+         output_dict['build_scripts'] = output_dict['build_scripts'] + int(d[c])
+      elif l in xml_l:
+         output_dict['xml'] = output_dict['xml'] + int(d[c])
 
-#get the common
-for branch in android_branches:
-   branch_path = initial_path + "/" + branch
-   os.chdir( initial_path )
-   output = CountLinesOfCode( branch_path, branch, high_level_dirs )
-   print output
+
+#Generate statistics per branch
+#Outputs coarse data for 5 catagories, code/comments/num files
+def GenerateStats( branch_names, branch_stats ):
+   out = {}
+
+   native = [ 'abi', 'bionic', 'external', 'bootable', 'hardware', 'system' ]
+   build_and_tools = [ 'build', 'device', 'prebuilts', 'vendor' ]
+   framework = [ 'development', 'dalvik', 'frameworks', 'libcore', 'libnativehelper' ]
+   apps = [ 'packages' ]
+   dev = [ 'cts', 'ndk', 'pdk', 'gdk', 'docs', 'sdk' ]
+
+   for branch in branch_names:
+      branch_dir_stats = branch_stats[branch]
+
+      native_d = {'java': 0, 'native': 0, 'build_scripts': 0, 'xml': 0}
+      build_and_tools_d = {'java': 0, 'native': 0, 'build_scripts': 0, 'xml': 0}
+      framework_d = {'java': 0, 'native': 0, 'build_scripts': 0, 'xml': 0}
+      apps_d = {'java': 0, 'native': 0, 'build_scripts': 0, 'xml': 0}
+      dev_d = {'java': 0, 'native': 0, 'build_scripts': 0, 'xml': 0}
+
+      for dir in branch_dir_stats:
+         if dir in native:
+            ParseStats( branch_dir_stats[dir], native_d )
+         elif dir in build_and_tools:
+            ParseStats( branch_dir_stats[dir], build_and_tools_d )
+         elif dir in framework:
+            ParseStats( branch_dir_stats[dir], framework_d )
+         elif dir in apps:
+            ParseStats( branch_dir_stats[dir], apps_d )
+         elif dir in dev:
+            ParseStats( branch_dir_stats[dir], dev_d )
+
+      stats = {}
+      stats['native'] = native_d
+      stats['build_and_tools'] = build_and_tools_d
+      stats['framework'] = framework_d
+      stats['apps'] = apps_d
+      stats['dev'] = dev_d
+
+      out[branch] = stats
+
+   return out
+
+
+def CalcNumFiles( stats ):
+
+   branch_summary = {}
+
+   for branch in stats:
+      branch_summary[branch] = 0
+      bstats = stats[branch] #actual stats for this branch
+      for c in classification: #iterate over the branch stats, per coarse grouping
+         tmp = bstats[c]
+         print branch, c, tmp       
+         if len(tmp) > 0:
+            for f in file_types:
+               branch_summary[branch] = branch_summary[branch] + tmp[f]
+
+   return branch_summary
+
+
+def CalcNumLinesCode( stats ):
+   print "a"
+
+
+##################
+## Main
+#################
+def main():
+
+   #assume that this is run from a directory where the android repos will be checked out
+   initial_path = os.getcwd()
+
+   #checkout the branches only if they don't exist already
+   for branch in android_branches:
+      branch_path = initial_path + "/" + branch
+      print branch_path
+
+      if not os.path.exists( branch_path ):
+         print "Branch does not exist - creating"
+         CheckoutRepo( branch_path, branch )
+         StripGitRepos( branch_path ) #this is just to save space
+
+   #get all the top level dirs from the last branch checked out
+   last_branch_path = initial_path + "/" + android_branches[-1]
+   high_level_dirs = HighLevelDirs( last_branch_path )
+   high_level_dirs.append('vendor')
+
+   #get all the lines of code from each branch
+   branch_stats = {}
+   for branch in android_branches:
+      branch_path = initial_path + "/" + branch
+      os.chdir( initial_path )
+      branch_stats[ branch ] = CountLinesOfCode( branch_path, branch, high_level_dirs )
+
+   #generate the stats
+   processed_stats = GenerateStats( android_branches, branch_stats )
+
+   #stats calc
+   print CalcNumFiles( processed_stats )
+   #CalcNumLinesCode( processed_stats )
+   #CalcNativeVSJava( processed_stats )
+
+#usual python main thing
+if __name__ == "__main__":
+    sys.exit(main())
